c90dc493f66e876a6eebcbd8f1298e276caaab62
FzyForever <55248707+FzyForever@users.noreply.github.com>
2020-09-16 02:51:34 +0800

    Update run_redis.py

--- lagou1/run_redis.py
+++ lagou1/run_redis.py
@@ -3,7 +3,6 @@ import json
 import requests
 
 rd = redis.Redis('127.0.0.1', port=6379)
-proxies={"http":"http://112.111.217.45:9999"}
 h1={'User-Agent': 'Opera/9.80 (iPhone; Opera Mini/7.1.32694/27.1407; U; en) Presto/2.8.119 Version/11.10',\
 'Referer': 'https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput='} #创建不同的headers
 h2={'User-Agent': 'Mozilla/5.0 (Linux; U; Android 2.0; en-us; Droid Build/ESD20) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17',\
